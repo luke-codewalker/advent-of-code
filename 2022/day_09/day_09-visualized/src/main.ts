a98a5a3768ae3dde4d7c7252876448071ea53268
Luke Codewalker <info@luke-codewalker.com>
2022-12-09 13:56:24 +0100

    Day 09 part 2 visualized

--- 2022/day_09/day_09-visualized/src/main.ts
+++ 2022/day_09/day_09-visualized/src/main.ts
@@ -7,67 +7,57 @@ canvas.height = 600
 
 const CHAR_SIZE = 20
 type Position = { x: number, y: number }
-const drawTail = (pos: Position) => {
+
+const drawChar = (char: string, pos: Position, withHighlight = false) => {
   const x = pos.x * CHAR_SIZE
   const y = pos.y * CHAR_SIZE
-  ctx.fillStyle = 'black'
-  ctx.fillRect(x, y, CHAR_SIZE, -CHAR_SIZE)
-  ctx.strokeStyle = '#aaa'
-  ctx.strokeRect(x, y, CHAR_SIZE, -CHAR_SIZE)
-  ctx.fillStyle = 'white'
 
+  if (withHighlight) {
+    ctx.fillStyle = 'black'
+    ctx.fillRect(x, y, CHAR_SIZE, -CHAR_SIZE)
+    ctx.strokeStyle = '#aaa'
+    ctx.strokeRect(x, y, CHAR_SIZE, -CHAR_SIZE)
+  }
+
+  ctx.fillStyle = 'white'
   ctx.font = `${CHAR_SIZE}px monospace`
   ctx.textAlign = 'center'
   ctx.textBaseline = 'middle'
-  ctx.fillText('T', x + CHAR_SIZE / 2, y - CHAR_SIZE / 2)
+  ctx.fillText(char, x + CHAR_SIZE / 2, y - CHAR_SIZE / 2)
+}
+
+const drawTail = (pos: Position) => {
+  drawChar('T', pos, true)
 }
 
 const drawHead = (pos: Position) => {
-  const x = pos.x * CHAR_SIZE
-  const y = pos.y * CHAR_SIZE
-  ctx.fillStyle = 'black'
-  ctx.fillRect(x, y, CHAR_SIZE, -CHAR_SIZE)
-  ctx.strokeStyle = '#aaa'
-  ctx.strokeRect(x, y, CHAR_SIZE, -CHAR_SIZE)
-  ctx.fillStyle = 'white'
-
-  ctx.font = `${CHAR_SIZE}px monospace`
-  ctx.textAlign = 'center'
-  ctx.textBaseline = 'middle'
-  ctx.fillText('H', x + CHAR_SIZE / 2, y - CHAR_SIZE / 2)
+  drawChar('H', pos, true)
 }
 
 const drawHeadVisited = (pos: Position) => {
-  const x = pos.x * CHAR_SIZE
-  const y = pos.y * CHAR_SIZE
-  ctx.fillStyle = 'white'
-  ctx.strokeStyle = '#aaa'
-
-  ctx.font = `${CHAR_SIZE}px monospace`
-  ctx.textAlign = 'center'
-  ctx.textBaseline = 'middle'
-  ctx.fillText('\u00B7', x + CHAR_SIZE / 2, y - CHAR_SIZE / 2)
+  drawChar('\u00B7', pos)
 }
 const drawTailVisited = (pos: Position) => {
-  const x = pos.x * CHAR_SIZE
-  const y = pos.y * CHAR_SIZE
-  ctx.fillStyle = 'white'
-  ctx.strokeStyle = '#aaa'
-
-  ctx.font = `${CHAR_SIZE}px monospace`
-  ctx.textAlign = 'center'
-  ctx.textBaseline = 'middle'
-  ctx.fillText('#', x + CHAR_SIZE / 2, y - CHAR_SIZE / 2)
+  drawChar('#', pos)
 }
 
 
-let head: Position = { x: 0, y: 0 }
-let tail: Position = { x: 0, y: 0 }
-
-const headPath: Position[] = [{ ...head }]
-const tailPath: Position[] = [{ ...tail }]
+const rope: [Position[], Position[], Position[], Position[], Position[], Position[], Position[], Position[], Position[], Position[],] = [
+  [{ x: 0, y: 0 }],
+  [{ x: 0, y: 0 }],
+  [{ x: 0, y: 0 }],
+  [{ x: 0, y: 0 }],
+  [{ x: 0, y: 0 }],
+  [{ x: 0, y: 0 }],
+  [{ x: 0, y: 0 }],
+  [{ x: 0, y: 0 }],
+  [{ x: 0, y: 0 }],
+  [{ x: 0, y: 0 }],
+]
 
 window.addEventListener('keypress', evt => {
+  const headPath = rope[0]
+  const head = { ...headPath.slice(-1)[0] }
   switch (evt.key) {
     case "w":
       head.y -= 1
@@ -86,6 +76,18 @@ window.addEventListener('keypress', evt => {
       headPath.push({ ...head })
       break;
   }
+
+  for (let i = 1; i < rope.length; i++) {
+    const headPath = rope[i - 1]
+    const tailPath = rope[i]
+
+    const head = headPath.slice(-1)[0]
+    const tail = tailPath.slice(-1)[0]
+
+    if (headPath.length > tailPath.length && dist(head, tail) >= 2) {
+      tailPath.push(headPath.slice(-2)[0])
+    }
+  }
 })
 
 const dist = (pos1: Position, pos2: Position): number => {
@@ -94,11 +96,10 @@ const dist = (pos1: Position, pos2: Position): number => {
 
 
 const draw = () => {
-  if (headPath.length > tailPath.length && dist(head, tail) >= 2) {
-    tailPath.push(headPath.slice(-2)[0])
-    tail = tailPath.slice(-1)[0]
-  }
-
+  const headPath = rope[0]
+  const head = headPath.slice(-1)[0]
+  const tailPath = rope[rope.length - 1]
+  const tail = tailPath.slice(-1)[0]
 
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   ctx.fillStyle = 'black'
@@ -113,6 +114,12 @@ const draw = () => {
   for (const t of tailPath) {
     drawTailVisited(t)
   }
+
+  for (let i = 1; i < rope.length - 1; i++) {
+    const pos = rope[i].slice(-1)[0]
+    drawChar(`${i + 1}`, pos, true)
+  }
+
   drawTail(tail)
   drawHead(head)
 
