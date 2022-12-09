de33f1fc02bc380b2b24cb6409366d9f3ae82e3b
Luke Codewalker <info@luke-codewalker.com>
2022-12-09 13:30:48 +0100

    Day 09 part 1

--- 2022/day_09/main.js
+++ 2022/day_09/main.js
@@ -1,5 +1,5 @@
 import { open } from 'node:fs/promises';
-const input = await open('./test.txt');
+const input = await open('./input.txt');
 
 const instructions = []
 for await (const line of input.readLines()) {
@@ -8,8 +8,7 @@ for await (const line of input.readLines()) {
     instructions.push({ direction, amount })
 }
 
-
-const move = (direction, head) => {
+const moveHead = (direction, head) => {
     let { x, y } = head;
     switch (direction) {
         case 'R':
@@ -28,18 +27,30 @@ const move = (direction, head) => {
             y--
             break;
     }
-
     return { x, y }
 }
 
+const headPath = [{ x: 0, y: 0 }]
+const tailPath = [{ x: 0, y: 0 }]
 
-
-const follow = (head, tail) => {
-    let { headX, headY } = head;
-    let { tailX, tailY } = tail;
-
-
+const dist = (pos1, pos2) => {
+    return Math.sqrt((pos1.x - pos2.x) ** 2 + (pos1.y - pos2.y) ** 2)
 }
 
-let head = { x: 0, y: 0 }
-let tail = { x: 0, y: 0 }
+
+for (const instruction of instructions) {
+    const { direction, amount } = instruction;
+    for (let i = 0; i < amount; i++) {
+        const head = headPath.slice(-1)[0]
+        const newHead = moveHead(direction, head)
+        headPath.push(newHead)
+        const tail = tailPath.slice(-1)[0]
+        if (headPath.length > tailPath.length && dist(newHead, tail) >= 2) {
+            tailPath.push(headPath.slice(-2)[0])
+        }
+    }
+}
+
+// Part 1
+const uniquePositions = new Set(tailPath.map(pos => `${pos.x},${pos.y}`))
+console.log(`There are ${uniquePositions.size} positions the tail visited at least once`);
