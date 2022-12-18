c146c0925d0f883b53c5c6176148ad3e8a008f75
Luke Codewalker <info@luke-codewalker.com>
2022-12-18 10:50:36 +0100

    Day 16 input parsing

--- 2022/day_16/src/main.ts
+++ 2022/day_16/src/main.ts
@@ -1,9 +1,32 @@
 import { open } from 'node:fs/promises';
-const INPUT_FILE = `${process.cwd()}/data/input.txt`;
+const INPUT_FILE = `${process.cwd()}/data/test.txt`;
 
 const file = await open(INPUT_FILE);
-
-for await (const line of file.readLines()) {
-
+type Valve = {
+    name: string,
+    flowRate: number,
+    open: boolean,
+    connections: string[]
 }
+
+class PipeSystem {
+    valves: Valve[] = []
+
+    addValve(description: string) {
+        const name = description.match(/Valve (\w+)\s/)?.[1]!;
+        const flowRate = parseInt(description.match(/rate=(\d+)/)?.[1]!);
+        const connections = description.match(/to valve[s]? (.+)/)?.[1].split(',')!;
+
+        this.valves.push({
+            open: false,
+            name, flowRate, connections
+        })
+    }
+}
+
+const pipes = new PipeSystem()
+for await (const line of file.readLines()) {
+    pipes.addValve(line)
+}
+
 
