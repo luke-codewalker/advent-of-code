5001c98183e25115714f5472604ad93aed49d6ac
Luke Codewalker <info@luke-codewalker.com>
2023-12-01 10:50:56 +0100

    add more to cli (messy)

--- cli/getInput.ts
+++ cli/getInput.ts
@@ -1,6 +1,10 @@
 const sessionCookie = await Deno.readTextFile(".session_cookie");
-const year = 2022;
-const day = 24;
+import { Eta } from "https://deno.land/x/eta@v3.0.3/src/index.ts";
+const viewpath = Deno.cwd() + "/cli/templates/";
+const eta = new Eta({ views: viewpath, cache: true });
+
+const year = 2023;
+const day = 1;
 const res = await fetch(`https://adventofcode.com/${year}/day/${day}/input`, {
   headers: {
     "Cookie": `session=${sessionCookie}`,
@@ -15,6 +19,24 @@ if (res.status !== 200) {
   Deno.exit();
 }
 
+const solutionFile = eta.render("./solution", {
+  day: day.toString().padStart(2, "0"),
+  year,
+});
+
 const text = await res.text();
-await Deno.mkdir(`./${year}/day_${day}/data`, { recursive: true });
-await Deno.writeTextFile(`./${year}/day_${day}/data/input.txt`, text);
+await Deno.mkdir(`./${year}/day_${day.toString().padStart(2, "0")}/data`, {
+  recursive: true,
+});
+await Deno.writeTextFile(
+  `./${year}/day_${day.toString().padStart(2, "0")}/data/input.txt`,
+  text,
+);
+await Deno.writeTextFile(
+  `./${year}/day_${day.toString().padStart(2, "0")}/data/test.txt`,
+  "### Replace with test data ###",
+);
+await Deno.writeTextFile(
+  `./${year}/day_${day.toString().padStart(2, "0")}/solution.ts`,
+  solutionFile,
+);
